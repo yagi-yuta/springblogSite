ccdef8a933c5c69b83f4408fed142c0d4d83b230
YJSNPI\3ma0408 <yagi0629yu@gmail.com>
2025-04-03 18:25:36 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/SpringBlogSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogSiteApplication.class, args);
+	}
+
+}
