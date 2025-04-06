8ae56565044e363595f943e7a68f49c8f42c6ac9
YJSNPI\3ma0408 <yagi0629yu@gmail.com>
2025-04-06 17:01:28 +0900

    @RequestParamでname,email,password,passwordConfirmを取得、ifで,passwordとpasswordConfirmが不一致の場合massage”パスワードが一致しません”を送り新規登録ページへ戻る処理、	@Autowired でHttpSession session;と	User account;を追記しました

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,49 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
+	@Autowired //@Autowired @Componentで保存した情報でインスタンス化（スプリングコンテナに保存されたクラスをもとにデータを生成）
+	HttpSession session;
+
+	@Autowired //@Autowired @Componentで保存した情報でインスタンス化（スプリングコンテナに保存されたクラスをもとにデータを生成）
+	User account;　//モデル作成からここ！
 
 	@GetMapping({ "/users/new" })
 	public String newUsers() {
 
-		return "usersadd";
+		return "userAdd"; //タスク① newメソッドは仕えない　なぜか仕様書にはその旨の指示があるが使用できないメソッドなので変更 new()→newUsers()
 	}
+
+	@PostMapping({ "/users/add" })
+	public String add(
+			@RequestParam(name = "name", defaultValue = "") String name,
+			@RequestParam(name = "email", defaultValue = "") String email,
+			@RequestParam(name = "password", defaultValue = "") String password,
+			@RequestParam(name = "passwordConfirm", defaultValue = "") String passwordConfirm,
+			Model model) {
+
+		if (!password.equals(passwordConfirm)) {
+			model.addAttribute("message", "パスワードが一致しません");
+			return "userAdd";
+		}
+		account.setName(name);
+		return "redirect:/login";
+	}
+
+	@GetMapping({ "/", "/login" })
+	public String index() {
+		return "login";
+	}
+
 }
+
+//!password.equals(passwordConfirm) !マークは否定の意味　passwordを参照してequalsで比較。何と比較？→(passwordConfirm)
