1e1a273537a4012b4169a8dd9be9a32f12002b4a
YJSNPI\3ma0408 <yagi0629yu@gmail.com>
2025-04-06 14:21:24 +0900

    機能実装：1	ユーザー登録フォームを表示する

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+	@GetMapping({ "/users/new" })
+	public String newUsers() {
+
+		return "usersadd";
+	}
 }
