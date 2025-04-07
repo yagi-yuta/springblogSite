a986f93654b973b0e3ddca035bfa7ea7184263f6
yagi-yuta <yagi0629yu@gmail.com>
2025-04-07 16:09:17 +0900

    Usesモデルを作成しました。今回は制作環境が社用PCでSSHpathが既に設定済みのためGitHub上で直接編集しています。

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,45 @@
 package com.example.demo.model;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
 public class User {
+	
+private String name;
+private String email;
+private String password;
+private String passwordConfirm;
+
+//ゲッター
+public String getName() {
+	return name;
+}
+public String getEmail() {
+	return email;
+}
+public String getPassword() {
+	return password;
+}
+public String getPasswordConfirm() {
+	return passwordConfirm;
+}
+
+
+//セッター
+
+public void setName(String name) {
+	this.name = name;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public void setPasswordConfirm(String passwordconfirm) {
+	this.passwordConfirm = passwordconfirm;
+}
 
 }
